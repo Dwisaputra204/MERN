96b2f1a9eb696a594391d2e606e714f866887b0d
Dwisaputra204 <dwisaputra204@gmail.com>
2020-12-29 10:36:16 +0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+
 import 'assets/scss/style.scss'
+import LandingPage from 'pages/LandingPage';
+
 
 function App() {
   return (
     <div className="App">
-      Hi iam dwi
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
     </div>
   );
 }
